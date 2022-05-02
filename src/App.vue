9d1cf37c9f385cb9197aacd3a6593e8339c69066
NewFronTierWorks <nftw@tuta.io>
2022-05-02 17:29:56 -0600

    reset site

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,47 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">Change</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="black"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Big Bold Impact"
+          class="shrink mr-2"
+          contain
+          src="..\src\assets\BBI-1920x1080.jpg"
+          transition="scale-transition"
+          width="100"
+        />
+
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://NewFronTierWorks.com"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">NFTW</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/LandingPage.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-container fluid style="height: 100%;">
+    <v-layout fill-height>
+      <v-row class="text-center" height="100%">
+        <v-col cols="12">
+          <v-img
+            :src="require('../assets/BBI-1920x1080.jpg')"
+            class="my-3"
+            contain
+            height="61.8%"
+          />
+        </v-col>
+
+        <v-col class="mb-4">
+          <h1 class="display-2 font-weight-bold mb-3">
+            Welcome to <br>BIG BOLD IMPACT
+          </h1>
+        </v-col>
+      </v-row>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+      ecosystem: [
+        {
+          text: 'vuetify-loader',
+          href: 'https://github.com/vuetifyjs/vuetify-loader',
+        },
+        {
+          text: 'github',
+          href: 'https://github.com/vuetifyjs/vuetify',
+        },
+        {
+          text: 'awesome-vuetify',
+          href: 'https://github.com/vuetifyjs/awesome-vuetify',
+        },
+      ],
+      importantLinks: [
+        {
+          text: 'Documentation',
+          href: 'https://vuetifyjs.com',
+        },
+        {
+          text: 'Chat',
+          href: 'https://community.vuetifyjs.com',
+        },
+        {
+          text: 'Made with Vuetify',
+          href: 'https://madewithvuejs.com/vuetify',
+        },
+        {
+          text: 'Twitter',
+          href: 'https://twitter.com/vuetifyjs',
+        },
+        {
+          text: 'Articles',
+          href: 'https://medium.com/vuetify',
+        },
+      ],
+      whatsNext: [
+        {
+          text: 'Explore components',
+          href: 'https://vuetifyjs.com/components/api-explorer',
+        },
+        {
+          text: 'Select a layout',
+          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
+        },
+        {
+          text: 'Frequently Asked Questions',
+          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+        },
+      ],
+    }),
+  }
+</script>
