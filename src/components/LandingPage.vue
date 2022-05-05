dfd5fa94ea9c5064ecbbc9f9666f4db2c122e54a
NewFronTierWorks <nftw@tuta.io>
2022-05-05 11:52:37 -0600

    add !

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -13,7 +13,7 @@
 
         <v-col class="mb-4">
           <h1 class="display-2 font-weight-bold mb-3">
-            Welcome to <br>BIG BOLD IMPACT
+            Welcome to <br>BIG BOLD IMPACT!
           </h1>
         </v-col>
       </v-row>
