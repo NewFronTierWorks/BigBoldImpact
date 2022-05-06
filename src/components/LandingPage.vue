e76b708bf9c8ce5143054145cf3752d195221f22
NewFronTierWorks <nftw@tuta.io>
2022-05-06 09:23:57 -0600

    the door

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -14,8 +14,31 @@
           <h1 class="mb-3">
             Welcome to <br>BIG BOLD IMPACT!
           </h1>
-          <v-btn dark color="blue" to="/classroom">Enter</v-btn>
+          <v-btn dark color="blue" @click="overlay = true">Enter</v-btn>
         </v-col>
+        <v-overlay
+          :z-index="zIndex"
+          :value="overlay"
+        >
+          <v-card>
+            <v-card-title><h2>Enter The Door</h2></v-card-title>
+            <v-img
+            src="https://nftw.mypinata.cloud/ipfs/QmNw1Ww7PSSViKBxadXYvF6TNaL7FB4eu41WWJbUpyT62W"
+            class="my-3 mx-auto"
+            contain
+            width="200px"
+          />
+
+            <v-card-text>To Enter You Must Own the NFT</v-card-text>
+          </v-card>
+          <v-btn
+            class="white--text mt-3"
+            color="teal"
+            @click="overlay = false"
+          >
+            Return
+          </v-btn>
+        </v-overlay>
       </v-row>
   </v-container>
 </template>
@@ -23,7 +46,9 @@
 <script>
   export default {
     name: 'LandingPage',
-
-   
+    data: () => ({
+      overlay: false,
+      zIndex: 0,
+    }),   
   }
 </script>
