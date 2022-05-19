1a790fa69f978a9df696b54c8654467eb4616d66
NewFronTierWorks <nftw@tuta.io>
2022-05-18 18:46:59 -0600

    card maker gets geometric

--- src/views/CardMaker.vue
+++ src/views/CardMaker.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="Card-Maker">
-    <v-card class="pa-3">
+    <v-card class="pa-3 mx-auto my-2 text-center" width="500px">
         <h1>Card Maker</h1>
-        <canvas id="canvas" width="1000" height="1000"></canvas>
+        <div class="mx-auto">
+
+            <canvas id="canvas"  width="1000" height="1000"></canvas>
+        </div>
     </v-card>
-    <v-card class="pa-3">
-    <input type="text" v-model="exampleContent" />
+    <v-card class="pa-3 mx-auto my-2 text-center" width="500px">
+    <v-text-field
+            label="Custom Message"
+            v-model="customMessage"
+          ></v-text-field>
     <br>
-    <span>{{ exampleContent }}</span>
     <v-card-actions>
         <v-spacer></v-spacer>
 
-    <v-btn @click="SaveImage">Save Image</v-btn>
+    <v-btn @click="addX">Add X</v-btn>
+    <v-btn @click="addY">Add Y</v-btn>
+    <v-btn @click="minusX">Minus X</v-btn>
+    <v-btn @click="minusY">Minus Y</v-btn>
+    <v-btn color="purple" dark @click="saveImage">Save Image</v-btn>
+        <v-spacer></v-spacer>
     </v-card-actions>
     </v-card>
   </div>
@@ -20,7 +30,10 @@
 <script>
 export default {
     data: () => ({
-    'exampleContent': 'Just Getting Started'
+    customMessage: 'Just Getting Started',
+    count: 100,
+    x: 100,
+    y: 100,
   }),
   methods: {
     updateCanvas () {
@@ -29,17 +42,70 @@ export default {
       ctx.clearRect(0,0,canvas.width,canvas.height);
       ctx.fillStyle = "black";
       ctx.font="30px Impact";
-      ctx.fillText(this.exampleContent,50,100);
+      ctx.fillText(this.customMessage,50,100);
       ctx.fillStyle = "white";
       ctx.fillText("BIG BOLD IMPACT - NFTW",50,200);
       ctx.fillStyle = "black";
       ctx.fillText("Official NFT",50,300);
+        ctx.fillStyle = "yellow"
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = "yellow"
+      ctx.beginPath();
+        ctx.arc(100, 100, 100, 0, 2 * Math.PI);
+        ctx.stroke();
     },
-    SaveImage () {
+    saveImage () {
         var canvas = document.getElementById('canvas');
         var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
         console.log(image)
         window.location.href=image; // it will save locally
+    },
+    addWord () {
+        var canvas = document.getElementById('canvas'),
+          ctx = canvas.getContext('2d');
+          ctx.fillStyle = "yellow";
+      ctx.fillText("BIG BOLD IMPACT - NFTW",50,this.count);
+      console.log(this.count);
+      this.count = this.count + 100;
+
+    },
+    addX () {
+        this.x = this.x + 100;
+        var canvas = document.getElementById('canvas'),
+          ctx = canvas.getContext('2d');
+          ctx.fillStyle = "yellow";
+      ctx.beginPath();
+        ctx.arc(this.x, this.y, 100, 0, 2 * Math.PI);
+        ctx.stroke();
+    },
+    addY () {
+        this.y = this.y + 100;
+        var canvas = document.getElementById('canvas'),
+          ctx = canvas.getContext('2d');
+          ctx.fillStyle = "yellow";
+      ctx.beginPath();
+        ctx.arc(this.x, this.y, 100, 0, 2 * Math.PI);
+        ctx.stroke();
+
+    },
+    minusX () {
+        this.x = this.x - 100;
+        var canvas = document.getElementById('canvas'),
+          ctx = canvas.getContext('2d');
+          ctx.fillStyle = "yellow";
+      ctx.beginPath();
+        ctx.arc(this.x, this.y, 100, 0, 2 * Math.PI);
+        ctx.stroke();
+    },
+    minusY () {
+        this.y = this.y - 100;
+        var canvas = document.getElementById('canvas'),
+          ctx = canvas.getContext('2d');
+          ctx.fillStyle = "yellow";
+      ctx.beginPath();
+        ctx.arc(this.x, this.y, 100, 0, 2 * Math.PI);
+        ctx.stroke();
+
     }
   },
   watch: {
@@ -55,7 +121,7 @@ export default {
 <style scoped>
 canvas{
   background: #c00000;
-  width:600px;
-  height:600px;
+  width:400px;
+  height:400px;
 }
 </style>
